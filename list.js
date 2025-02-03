9a788e39dc58e09661ffe601ee63e4885ff2c236
sophiaes09 <sosp0001@stud.kea.dk>
2025-02-03 15:01:14 +0100

    ændringer

--- list.js
+++ list.js
@@ -1,6 +1,10 @@
+const mycategory = new URLSearchParams(window.location.search).get("category");
 const product_list_container = document.querySelector(".product_list_container");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
+const overskrift = document.querySelector("h1");
+overskrift.innerHTML = mycategory;
+
+fetch(`https://kea-alt-del.dk/t7/api/products/?category=${mycategory}`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
